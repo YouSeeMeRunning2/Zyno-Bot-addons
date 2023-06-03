26554ceea2c1daa7f88e85f627f2631a5cbd6a8a
Luuk <70959239+Luuk-Dev@users.noreply.github.com>
2023-06-03 22:47:11 +0200

    Create index.js

---
+++ index.js
@@ -0,0 +1,18 @@
+const { PermissionFlagsBits, EmbedBuilder } = require('discord.js');
+const numberTypes = require('./src/utils/numberTypes.js');
+
+module.exports = {
+    bitfield: require('./src/bitfields/scopes.js').bitfield,
+    Addon: require('./src/classes/addon.js'),
+    CommandBuilder: require('./src/classes/builders/commandBuilder.js'),
+    CommandOptionsBuilder: require('./src/classes/builders/commandOptionsBuilder.js'),
+    CommandOptionChoiceBuilder: require('./src/classes/builders/commandOptionChoiceBuilder.js'),
+    createBitfield: require('./src/utils/functions.js').createBitfield,
+    permissionsBitfield: PermissionFlagsBits,
+    ...numberTypes,
+    Embed: EmbedBuilder,
+    ButtonBuilder: require('./src/classes/builders/buttonBuilder.js'),
+    SelectMenuBuilder: require('./src/classes/builders/selectMenuBuilder.js'),
+    SelectMenuOptionBuilder: require('./src/classes/builders/selectMenuOptionBuilder.js'),
+    ActionRowBuilder: require('./src/classes/builders/actionRowBuilder.js')
+}
