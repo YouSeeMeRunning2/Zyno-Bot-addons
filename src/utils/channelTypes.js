7eaaf768a35eb35f1dd2df00b1a4d2fa560df450
Luuk <70959239+Luuk-Dev@users.noreply.github.com>
2023-06-03 22:49:04 +0200

    Create channelTypes.js

---
+++ src/utils/channelTypes.js
@@ -0,0 +1,15 @@
+var channels = {
+	'0': 'GuildText',
+	'1': 'DM',
+	'2': 'GuildVoice',
+	'3': 'GroupDM',
+	'4': 'GuildCategory',
+	'5': 'GuildAnnouncement',
+	'10': 'AnnouncementThread',
+	'11': 'PublicThread',
+	'12': 'PrivateThread',
+	'13': 'GuildStageVoice',
+	'14': 'GuildDirectory',
+	'15': 'GuildForum'
+};
+module.exports = channels;
