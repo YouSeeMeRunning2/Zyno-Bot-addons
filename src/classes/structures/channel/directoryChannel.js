afc79c2160d9cc1ab6b33dc507ddf44c6501ff1a
Luuk <70959239+Luuk-Dev@users.noreply.github.com>
2023-06-03 22:59:36 +0200

    Create directoryChannel.js

---
+++ src/classes/structures/channel/directoryChannel.js
@@ -0,0 +1,30 @@
+const BaseChannel = require('./base.js');
+const { validatePermission, getAddonPermission } = require('../../../utils/functions.js');
+const scopes = require('../../../bitfields/scopes.js');
+
+class DirectoryChannel extends BaseChannel{
+    constructor(data, addon, guild){
+        super(data, addon);
+        this.guild = guild;
+        this.name = data.name;
+        if(guild) guild.channels.set(this.id, this);
+        if(validatePermission(getAddonPermission(addon.name), scopes.bitfield.CHANNELS)){
+            addon.channels.set(this.id, this);
+        }
+        this.delete = function(){
+            return new Promise((resolve, reject) => {
+                if(!validatePermission(getAddonPermission(addon.name), scopes.bitfield.CHANNELS)) return reject(`Missing channels scope in bitfield`);
+            	data.delete().then(() => resolve()).catch(reject);
+            });
+        };
+        this.update = function(){
+            return new Promise((resolve, reject) => {
+                data.fetch().then(ch => {
+                    resolve(new DirectoryChannel(ch, addon, guild));
+                }).catch(reject);
+            });
+        };
+    }
+}
+
+module.exports = DirectoryChannel;
