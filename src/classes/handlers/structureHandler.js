25262f4967960a3e61ee13160d0bf12cb45712fb
Luuk <70959239+Luuk-Dev@users.noreply.github.com>
2023-08-17 15:21:53 +0200

    Create structureHandler.js

---
+++ src/classes/handlers/structureHandler.js
@@ -0,0 +1,61 @@
+const Member = require('../structures/member.js');
+const Save = require('../save.js');
+const TextChannel = require('../structures/channel/textChannel.js');
+const CategoryChannel = require('../structures/channel/categoryChannel.js');
+const VoiceChannel = require('../structures/channel/voiceChannel.js');
+const StageChannel = require('../structures/channel/stageChannel.js');
+const ForumChannel = require('../structures/channel/forumChannel.js');
+const DirectoryChannel = require('../structures/channel/directoryChannel.js');
+const BanEntry = require('../structures/entries/ban.js');
+const KickEntry = require('../structures/entries/kick.js');
+const MuteEntry = require('../structures/entries/mute.js');
+const BaseEntry = require('../structures/entries/base.js');
+const Message = require('../structures/message.js');
+const Guild = require('../structures/guild.js');
+const Emoji = require('../structures/emoji.js');
+const Role = require('../structures/role.js');
+const User = require('../structures/user.js');
+const Reaction = require('../structures/reaction.js');
+const ButtonInteraction =  require('../structures/interactions/buttonInteraction.js');
+const MenuInteraction = require('../structures/interactions/menuInteraction.js');
+const Mentions = require('../structures/mentions.js');
+
+const structures = {
+    Member: Member,
+    Save: Save,
+    TextChannel: TextChannel,
+    CategoryChannel: CategoryChannel,
+    VoiceChannel: VoiceChannel,
+    StageChannel: StageChannel,
+    ForumChannel: ForumChannel,
+    DirectoryChannel: DirectoryChannel,
+    BanEntry: BanEntry,
+    KickEntry: KickEntry,
+    MuteEntry: MuteEntry,
+    BaseEntry: BaseEntry,
+    Message: Message,
+    Guild: Guild,
+    Role: Role,
+    Emoji: Emoji,
+    User: User,
+    Reaction: Reaction,
+    ButtonInteraction: ButtonInteraction,
+    MenuInteraction: MenuInteraction,
+    Mentions: Mentions
+}
+
+class StructureHandler{
+    constructor(){}
+
+    createStructure(name, args){
+        if(!structures[name]) return;
+        let _arguments = [...(args || [])];
+        if(name !== 'Save') _arguments.push(this);
+        const structure = new structures[name](..._arguments);
+        return structure;
+    }
+}
+
+const structureHandler = new StructureHandler();
+
+module.exports = structureHandler;
