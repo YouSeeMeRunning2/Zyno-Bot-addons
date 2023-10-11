41fc631eb90bd5f340d04181b8d86960f03772b4
Luuk <70959239+Luuk-Dev@users.noreply.github.com>
2023-10-11 21:31:13 +0200

    Update clientParser.js

--- src/classes/handlers/clientParser.js
+++ src/classes/handlers/clientParser.js
@@ -3,13 +3,16 @@ const { Client } = require('discord.js');
 const { commandListeners, addons } = require('../../utils/saves.js');
 const Command = require('../command.js');
 const { handleEvents, createStructures } = require('./eventHandler.js');
-const { validatePermission } = require('../../utils/functions.js');
+const { validatePermission, passClientParser } = require('../../utils/functions.js');
 const bitfields = require('../../bitfields/scopes.js');
 var client = null;
+
+let receivedAddons = [];
 
 class ClientParser extends EventEmitter{
     constructor(){
         super();
+        passClientParser(this);
         this.once('ready', async () => {
             this.client_ready = true;
             if(this.interactionHandler !== null){
@@ -37,6 +40,10 @@ class ClientParser extends EventEmitter{
             }
             resolve();
         });
+    }
+    parseAddonRegistar(addonRegistar){
+        this.addonRegistar = addonRegistar;
+        this.emit('addonRegistarReady', this.addonRegistar);
     }
     commandAvailability(data, interaction){
         return new Promise(async (resolve, reject) => {
@@ -75,10 +82,66 @@ class ClientParser extends EventEmitter{
             resolve();
         });
     }
+    parseAddons(addons, force = false){
+        return new Promise(async (resolve, reject) => {
+            if(!Array.isArray(addons)) return reject(`Invalid addon array`);
+            const addonsArr = this.addons.toReadableArray();
+            for(let i = 0; i < addons.length; i++){
+                await new Promise(_resolve => {
+                    let addonName = addons[i];
+                    let resolveableName = require.resolve(addonName);
+                    if(addonsArr.filter(a => a.value.resolveablePath === resolveableName).length > 0){
+                        _resolve(true);
+                    } else {
+                        this.once('addonEmit',async (addon, callback) => {
+                            clearTimeout(this.waitableTimeout[resolveableName]);
+                            Object.defineProperty(addon, 'resolveablePath', {
+                                value: resolveableName,
+                                writable: false,
+                                configurable: false
+                            });
+                            this.removeAllListeners('addonEmit');
+                            receivedAddons.push(resolveableName);
+                            callback(addon.name, true, function(){
+                                if(force) _resolve(true);
+                            });
+                            if(!force) _resolve(true);
+                        });
+                        try{
+                            require(resolveableName);
+                        } catch (err){
+                            console.log(`Error while starting addon:`, err);
+                        }
+                        setTimeout(() => {
+                            if(receivedAddons.indexOf(resolveableName) >= 0) return;
+                            delete require.cache[resolveableName];
+                            this.removeAllListeners('addonEmit');
+                            _resolve(false);
+                        }, 3e3);
+                    }
+                });
+            }
+            resolve();
+        });
+    }
+    emitAddon(addon){
+        return new Promise((resolve, reject) => {
+            this.emit('addonEmit', addon, function(name, bool, callback){
+                if(name === addon.name && bool) resolve(callback);
+                else{
+                    callback();
+                    reject(`Due to security reasons, this addon was blocked`);
+                }
+            });
+        });
+    }
     client_ready = false;
     ready = false;
     InteractionHandler = null;
     event = new EventEmitter();
+    waitableTimeout = {}
+    addons = addons;
+    addonRegistar = undefined;
 }
 
 module.exports = ClientParser;
