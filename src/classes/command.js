48509fa0bebbaf46fc72feb7e61dd17c018bca76
Luuk <70959239+Luuk-Dev@users.noreply.github.com>
2023-12-01 22:51:48 +0100

    Update command.js

--- src/classes/command.js
+++ src/classes/command.js
@@ -1,12 +1,22 @@
 const Save = require('./save.js');
-const Member = require('./structures/member.js');
 const User = require('./structures/user.js');
 const Role = require('./structures/role.js');
 const { getClientParser, getClient } = require('../utils/functions.js');
 const { getMessageContent } = require('../utils/messageFunctions.js');
 const { ApplicationCommandOptionType, ChannelType } = require('discord.js');
+const channelManager = require('./managers/channelManager.js');
+const roleManager = require('./managers/roleManager.js');
+const userManager = require('./managers/userManager.js');
+const memberManager = require('./managers/guildMemberManager.js');
+const guildManager = require('./managers/guildManager.js');
+const messageManager = require('./managers/messageManager.js');
 
-function commandResolver(data, save, structureHandler, addonData){
+function commandResolver(data, save, guild, structureHandler, addonData, defaultData){
+    const addonChannelManager = channelManager.get(addonData.addon.name) || new Save();
+    const guildChannelManager = addonChannelManager.get(defaultData.guild.id) || new Save();
+    const addonRoleManager = roleManager.get(addonData.addon.name) || new Save();
+    const guildRoleManager = addonRoleManager.get(defaultData.guild.id) || new Save();
+    const addonUserManager = userManager.get(addonData.addon.name) || new Save();
     return data.reduce((arr, item) => {
         var add;
         if(item.type === ApplicationCommandOptionType.Subcommand || item.type === ApplicationCommandOptionType.SubcommandGroup){
@@ -23,26 +33,32 @@ function commandResolver(data, save, structureHandler, addonData){
         } else if(item.type === ApplicationCommandOptionType.Channel){
             let channel;
             if(item.channel.type === ChannelType.GuildText || item.channel.type === ChannelType.GuildAnnouncement){
-                channel = structureHandler.createStructure('TextChannel', [item.channel, addonData.addon, this.guild]);
+                channel = guildChannelManager.get(item.channel.id);
+                if(!channel) channel = structureHandler.createStructure('TextChannel', [item.channel, addonData.addon, guild]);
             } else if(item.channel.type === ChannelType.GuildCategory){
-                channel = structureHandler.createStructure('CategoryChannel', [item.channel, addonData.addon, this.guild]);
+                channel = guildChannelManager.get(item.channel.id);
+                if(!channel) channel = structureHandler.createStructure('CategoryChannel', [item.channel, addonData.addon, guild]);
             } else if(item.channel.type === ChannelType.GuildVoice){
-                channel = structureHandler.createStructure('VoiceChannel', [item.channel, addonData.addon, this.guild]);
+                channel = guildChannelManager.get(item.channel.id);
+                if(!channel) channel = structureHandler.createStructure('VoiceChannel', [item.channel, addonData.addon, guild]);
             } else if(item.channel.type === ChannelType.GuildStageVoice){
-                channel = structureHandler.createStructure('StageChannel', [item.channel, addonData.addon, this.guild]);
+                channel = guildChannelManager.get(item.channel.id);
+                if(!channel) channel = structureHandler.createStructure('StageChannel', [item.channel, addonData.addon, guild]);
             } else if(item.channel.type === ChannelType.GuildForum){
-                channel = structureHandler.createStructure('ForumChannel', [item.channel, addonData.addon, this.guild]);
+                channel = guildChannelManager.get(item.channel.id);
+                if(!channel) channel = structureHandler.createStructure('ForumChannel', [item.channel, addonData.addon, guild]);
             } else if(item.channel.type === ChannelType.GuildDirectory){
-                channel = structureHandler.createStructure('DirectoryChannel', [item.channel, addonData.addon, this.guild]);
+                channel = guildChannelManager.get(item.channel.id);
+                if(!channel) channel = structureHandler.createStructure('DirectoryChannel', [item.channel, addonData.addon, guild]);
             }
             add = channel.string;
             save.set(item.name, channel);
         } else if(item.type === ApplicationCommandOptionType.Role){
-            let role = new Role(item.role, addonData.addon, this.guild)
+            let role = guildRoleManager.get(item.role.id) ?? new Role(item.role, addonData.addon, guild)
             add = role.string;
             save.set(item.name, role);
         } else if(item.type === ApplicationCommandOptionType.User){
-            let user = new User(item.user, addonData.addon, false, structureHandler);
+            let user = addonUserManager.get(item.user.id) ?? new User(item.user, addonData.addon, false, structureHandler);
             add = user.string;
             save.set(item.name, user);
         }
@@ -58,10 +74,12 @@ function commandResolver(data, save, structureHandler, addonData){
 class Command{
 	constructor(data, interaction, registeredCommandData, addonData, structureHandler){
         let client = getClient();
+        this.addon = addonData.addon;
         this.name = registeredCommandData.name;
         this.description = registeredCommandData.description;
         this.slashCommand = interaction === true;
-        this.member = new Member(data.member, addonData.addon, structureHandler);
+        this.memberId = data.member?.id;
+        this.guildId = data.guild?.id;
         this.created = new Date();
         this.createdTimestamp = this.created.getTime();
         this.reply = (...content) => {
@@ -79,12 +97,13 @@ class Command{
                 }
             });
         }
-        this.guild = this.member.guild;
-        this.channel = this.guild.channels.get(data.channelId);
-        this.message = interaction === false ? structureHandler.createStructure('Message', [data, addonData.addon]) : undefined;
+        this.channelId = data.channelId;
+        this.messageId = interaction === false ? data.id : undefined;
         if(interaction === false) this.args.push(...(data.content || '').substring(client.config.prefix.length).split(' '));
         else {
-            let args = commandResolver(data.options.data, this.options, structureHandler, addonData);
+            const addonGuildManager = guildManager.get(this.addon.name) || new Save();
+            const g = addonGuildManager.get(this.guildId);
+            let args = commandResolver(data.options.data, this.options, g, structureHandler, addonData, data);
             this.args = [registeredCommandData.name, ...args];
         }
         this.isSlashCommand = () => {
@@ -108,6 +127,27 @@ class Command{
                 resolve();
             });
         }
+    }
+    get channel(){
+        const addonChannelManager = channelManager.get(this.addon.name) || new Save();
+        const guildChannelManager = addonChannelManager.get(this.guildId) || new Save();
+        return guildChannelManager.get(this.channelId);
+    }
+    get guild(){
+        const addonGuildManager = guildManager.get(this.addon.name) || new Save();
+        return addonGuildManager.get(this.guildId);
+    }
+    get member(){
+        const addonMemberManager = memberManager.get(this.addon.name) || new Save();
+        const guildMemberManager = addonMemberManager.get(this.guildId) || new Save();
+        return guildMemberManager.get(this.memberId);
+    }
+    get message(){
+        if(this.slashCommand === true) return undefined;
+        const addonMessageManager = messageManager.get(this.addon.name) || new Save();
+        const guildMessageManager = addonMessageManager.get(this.guildId) || new Save();
+        const channelMessageManager = guildMessageManager.get(this.channelId) || new Save();
+        return channelMessageManager.get(this.messageId);
     }
     name = null;
     description = null;
