b5af3a4310b00bee09695e805ef07a41d0e63267
Luuk <70959239+Luuk-Dev@users.noreply.github.com>
2023-12-01 22:56:05 +0100

    Update mute.js

--- src/classes/structures/entries/mute.js
+++ src/classes/structures/entries/mute.js
@@ -1,9 +1,11 @@
-const User = require('../user.js');
+const userManager = require('../../managers/userManager.js');
+const Save = require('../../save.js');
 
 class MuteEntry{
     constructor(data, member, addon){
-        this.executor = new User(data.executor, addon, false);
-        this.user = new User(data.target, addon, false);
+        const addonUserManager = userManager.get(addon.name) || new Save();
+        this.executor = addonUserManager.get(data.executor.id);
+        this.user = addonUserManager.get(data.target.id);
         this.reason = data.reason;
         this.muted = new Date(data.createdTimestamp);
         this.mutedTimestamp = data.createdTimestamp;
