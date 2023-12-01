8f0ff8f26d88341c5267ae3d54b648f6ed113d47
Luuk <70959239+Luuk-Dev@users.noreply.github.com>
2023-12-01 22:55:48 +0100

    Update base.js

--- src/classes/structures/entries/base.js
+++ src/classes/structures/entries/base.js
@@ -1,9 +1,11 @@
-const User = require('../user.js');
+const userManager = require('../../managers/userManager.js');
+const Save = require('../../save.js');
 
 class BaseEntry{
     constructor(data, member, guild, addon){
-        this.executor = new User(data.executor, addon, false);
-        this.user = member ? new User(data.target, addon, false) : undefined;
+        const addonUserManager = userManager.get(addon.name) || new Save();
+        this.executor = addonUserManager.get(data.executor.id);
+        this.user = member ? addonUserManager.get(data.target.id) : undefined;
         this.reason = data.reason;
         this.changed = new Date(data.createdTimestamp);
         this.changedTimestamp = data.createdTimestamp;
