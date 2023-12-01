b866b51b542d19f41d39ca6b013d80df118cd070
Luuk <70959239+Luuk-Dev@users.noreply.github.com>
2023-12-01 22:56:50 +0100

    Update directoryChannel.js

--- src/classes/structures/channel/directoryChannel.js
+++ src/classes/structures/channel/directoryChannel.js
@@ -1,13 +1,21 @@
 const BaseChannel = require('./base.js');
 const { validatePermission, getAddonPermission } = require('../../../utils/functions.js');
 const scopes = require('../../../bitfields/scopes.js');
+const channelManager = require('../../managers/channelManager.js');
+const Save = require('../../save.js');
 
 class DirectoryChannel extends BaseChannel{
-    constructor(data, addon, guild, structureHandler){
+    constructor(data, addon, guild, structureHandler, cache){
         super(data, addon);
+        if(cache){
+            const addonChannelManager = channelManager.get(addon.name) || new Save();
+            const guildChannelManager = addonChannelManager.get(guild.id) || new Save();
+            guildChannelManager.set(data.id, this);
+            addonChannelManager.set(guild.id, guildChannelManager);
+            channelManager.set(addon.name, addonChannelManager);
+        }
         this.guild = guild;
         this.name = data.name;
-        if(guild) guild.channels.set(this.id, this);
         if(validatePermission(getAddonPermission(addon.name), scopes.bitfield.CHANNELS)){
             addon.channels.set(this.id, this);
         }
