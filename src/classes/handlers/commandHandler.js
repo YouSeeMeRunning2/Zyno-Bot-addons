eae2d94ef6d19927e7337c7ca744187d9f8ced72
Luuk <70959239+Luuk-Dev@users.noreply.github.com>
2023-12-01 23:01:25 +0100

    Update commandHandler.js

--- src/classes/handlers/commandHandler.js
+++ src/classes/handlers/commandHandler.js
@@ -15,12 +15,42 @@ class CommandHandler extends EventEmitter{
             id: {
                 value: data.id,
                 writable: false
+            },
+            options: {
+                value: data.options,
+                writable: false
+            },
+            category: {
+                value: data.category,
+                writable: false
+            }, 
+            dm_permission: {
+                value: data.dm_permission,
+                writable: false
+            },
+            permissions: {
+                value: data.default_member_permissions,
+                writable: false
+            },
+            nsfw: {
+                value: data.nsfw,
+                writable: false
+            },
+            overwrite: {
+                value: data.overwrite,
+                writable: false
             }
         });
     }
     name = null;
     description = null;
     id = null;
+    options = [];
+    category = null;
+    dm_permission = false;
+    permissions = null;
+    nsfw = false;
+    overwrite = false;
 }
 
 module.exports = CommandHandler;
