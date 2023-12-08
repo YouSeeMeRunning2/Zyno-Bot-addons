4e5476ec2ca2b552c51522cddaffd23aab3f8592
Luuk <70959239+Luuk-Dev@users.noreply.github.com>
2023-12-08 22:23:51 +0100

    Update guild.js

--- src/classes/structures/guild.js
+++ src/classes/structures/guild.js
@@ -48,6 +48,7 @@ class Guild{
         this.created = new Date(guild.createdTimestamp);
         this.createdTimestamp = guild.createdTimestamp;
         this.everyoneRoleId = guild.roles.everyone.id;
+        this.boosts = data.premiumSubscriptionCount;
         if(validatePermission(getAddonPermission(addon.name), scopes.bitfield.GUILDS)){
             addon.guilds.set(this.id, this);
         }
