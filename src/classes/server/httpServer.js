8c4dd43a370c52f87cb72ce4578574f5bb90ba9f
Luuk <70959239+Luuk-Dev@users.noreply.github.com>
2023-12-28 16:06:46 +0100

    Update httpServer.js

--- src/classes/server/httpServer.js
+++ src/classes/server/httpServer.js
@@ -2,6 +2,7 @@ const http = require('http');
 const EventEmitter = require('events');
 const RequestInfo = require('./requestInfo.js');
 const ResponseInfo = require('./responseInfo.js');
+const { formidable } = require('formidable');
 
 let HTTPserver;
 
@@ -9,7 +10,7 @@ class HttpServer{
     constructor(port){
         HTTPserver = http.createServer({
             requestTimeout: 1e4
-        }, (req, res) => {
+        }, async (req, res) => {
             res.setHeader('X-Powered-By', 'Zyno Bot');
             if((req.method || '').toLowerCase() === 'get'){
                 let getEvents = [...this.get.eventNames()];
@@ -24,17 +25,25 @@ class HttpServer{
                         let eventRegEx = new RegExp(eventName.split("{*}").join("(\\w+)"), "i");
                         let regExRes = eventRegEx.exec((req.url || ''));
                         placeholders.push(...regExRes.slice(1, regExRes.length));
-                        this.get.emit(eventName, new RequestInfo(req, null, placeholders), new ResponseInfo(res));
+                        this.get.emit(eventName, new RequestInfo(req, {}, placeholders), new ResponseInfo(res));
                     }
                 }
-                if(getEvents.indexOf((req.url || '')) >= 0){
-                    this.get.emit((req.url || '').split('?')[0], new RequestInfo(req, null, []), new ResponseInfo(res));
+                if(getEvents.indexOf((req.url || '').split('?')[0]) >= 0){
+                    this.get.emit((req.url || '').split('?')[0], new RequestInfo(req, {}, []), new ResponseInfo(res));
                 }
             } else if((req.method || '').toLowerCase() === 'post'){
-                let body = [];
-                req.on('data', chunk => {
-                    body.push(chunk.toString());
-                });
+                let body = {};
+                const form = formidable({});
+                let fields = {};
+                try{
+                    [fields] = await form.parse(req);
+                } catch(err) {
+                    res.end('Error while parsing body');
+                    return;
+                }
+                for(const fieldName in fields){
+                    body[fieldName] = fields[fieldName][0];
+                }
                 req.on('end', () => {
                     let postEvents = [...this.post.eventNames()];
                     let placeholderPostEvents = postEvents.filter(e => e.indexOf("{*}") >= 0).filter(e => {
@@ -48,11 +57,11 @@ class HttpServer{
                             let eventRegEx = new RegExp(eventName.split("{*}").join("(\\w+)"), "i");
                             let regExRes = eventRegEx.exec((req.url || ''));
                             placeholders.push(...regExRes.slice(1, regExRes.length));
-                            this.post.emit(eventName, new RequestInfo(req, body.join(''), placeholders), new ResponseInfo(res));
+                            this.post.emit(eventName, new RequestInfo(req, body, placeholders), new ResponseInfo(res));
                         }
                     }
-                    if(postEvents.indexOf((req.url || '')) >= 0){
-                        this.post.emit((req.url || '').split('?')[0], new RequestInfo(req, body.join(''), []), new ResponseInfo(res));
+                    if(postEvents.indexOf((req.url || '').split('?')[0]) >= 0){
+                        this.post.emit((req.url || '').split('?')[0], new RequestInfo(req, body, []), new ResponseInfo(res));
                     }
                 })
             } else {
